92ee5b411a9e290d04a570ddce0ac7ed43b18569
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-05-03 13:03:17 +0300

    added event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//Here I decleared the variables as the first step
+const form = document.getElementById("create-task-form");
+const task = document.getElementById("new-task-description");
+const output = document.getElementById("tasks");
+
+//Here I added an event listener capture the user's input and display it on the tasks list.
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const li = document.createElement("li");
+  li.textContent = task.value;
+
+  output.appendChild(li);
 });
